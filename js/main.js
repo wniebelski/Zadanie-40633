57822b31b804f4d4bebcf51f92e97dc2075954c2
wniebelski <wniebelski@gmail.com>
2022-09-27 21:31:54 +0200

    literówka

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let pierwszy = 'Uwielbiam JavaScript';
 let drugi = 'Jestem świetnym programistą';
 function porownaj (par1, par2){
-    if (par1.lenght > par2.lenght) {
+    if (par1.length > par2.length) {
         console.log(par1)
     }else {
         console.log(par2)
