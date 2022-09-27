b52ef71204601e079f238fd808aa22277249ae95
wniebelski <wniebelski@gmail.com>
2022-09-27 08:11:40 +0200

    zmiana

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let pierwszy = 'Uwielbiam JavaScript';
 let drugi = 'Jestem świetnym programistą';
 function porownaj (par1, par2){
     if (par1.lenght > par2.lenght) {
-        console.log(pat1)
+        console.log(par1)
     }else {
         console.log(par2)
     }
